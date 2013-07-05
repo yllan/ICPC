4505dc2bafad89240781cd291bb98313d323d782
yllan <yllan@me.com>
2013-07-06 02:33:09 +0800

    Add comment and reformat the code

--- 2013/D.cpp
+++ 2013/D.cpp
@@ -13,35 +13,25 @@ typedef char Factorization[20];
 int primes[20] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
 Factorization factorial[64];
 
-void divide(char *a, char *b)
+void divide(Factorization a, Factorization b)
 {
-  for (int i = 0; i < 20; i++)
-    a[i] -= b[i];
-}
-
-void print(Factorization f)
-{
-  for (int i = 0; i < 20; i++) printf("%d ", f[i]);
-  puts("");
+  for (int i = 0; i < 20; i++) a[i] -= b[i];
 }
 
 // Calculate f(k)
 long long f(MultiFactorial k)
 {
   // Only one exponent.
-  if (k[1] == 0)
-    return 1LL;
+  if (k[1] == 0) return 1LL;
 
-  int sum = 0;  
-  for (char *p = k; *p; p++) 
-    sum += *p;
+  int sum = 0;
+  for (char *p = k; sum += *p, *p; p++);
 
   // Calculate the multi-factorial (sum!) / (k.exponent[0]! * k.exponent[1]! * ... )
   Factorization multifact;
   memmove(multifact, factorial[sum], 20);
 
-  for (char *p = k; *p; p++)
-    divide(multifact, factorial[*p]);
+  for (char *p = k; divide(multifact, factorial[*p]), *p; p++);
 
   long long result = 1LL;
   for (int i = 0; i < 20; i++)
@@ -50,6 +40,7 @@ long long f(MultiFactorial k)
   return result;
 }
 
+// Generate the factorization of factorial. Ex. 4! = 2^3 * 3^1.
 void generate_factorial()
 {
   factorial[2][0] = 1;
@@ -64,6 +55,20 @@ void generate_factorial()
   }
 }
 
+// Generate combinatorial of factorization where the exponent is decreasing.
+// 2^1
+//
+// 2^2
+// 2^1 * 3^1
+//
+// 2^3
+// 2^2 * 3^1
+//
+// 2^4
+// 2^3 * 3^1
+// 2^2 * 3^2
+// 2^2 * 3^1 * 5^1
+// 2^1 * 3^1 * 5^1 * 7^1 
 void generate_candidate_recursive(char buf[20], int pos, int left, long long value)
 {
   if (left == 0) {
@@ -78,6 +83,7 @@ void generate_candidate_recursive(char buf[20], int pos, int left, long long val
     long long p_k = 1LL; // primes[pos] ^ k
     for (int k = 1; k <= upperbound; k++) {
       p_k *= primes[pos];
+      // The following break prevents overflow
       if (p_k < 0) break;
       if (value > 1 + LLONG_MAX / p_k) break;
       if (value * p_k < value) break;
